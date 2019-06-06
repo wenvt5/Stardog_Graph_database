18a852f4ef980f75b45acc94dc89ba80ad51deb2
Ying Wen <wenyingying@hotmail.com>
2019-06-06 16:40:15 -0400

    update values to be consistent with foreign keys

--- insert_rows_GO28915.sql
+++ insert_rows_GO28915.sql
@@ -1,14 +1,14 @@
-insert into Ea values ("GO28915","521948","16602491","SCRN","05850005B0012R","S4371413","Q2SO","RNA",,"OTHER; SPECIFY BELOW:LEFT ADRENAL""FALSE");
-insert into Ea values ("GO28915","586042","10096147","SCRN","05850008B0017R","S4371412","Q2SO","RNA","LUNG","FALSE");
+insert into Ea values ("GO28915","521948","16602491","SCRN","05850005B0012R","05850006B","Q2SO","RNA",,"OTHER; SPECIFY BELOW:LEFT ADRENAL""FALSE");
+insert into Ea values ("GO28915","586042","10096147","SCRN","05850008B0017R","05850007B","Q2SO","RNA","LUNG","FALSE");
 
-insert into Fmi values ("GO28915","585456","10677947","SCRN","TRF155914","S4413866","FDMI","DNA","LUNG","FALSE");
-insert into Fmi values ("GO28915","541719","15891411","SCRN","TRF155919","S4406328""FDMI","DNA","LUNG","FALSE");
+insert into Fmi values ("GO28915","585456","10677947","SCRN","TRF155914","05850006B","FDMI","DNA","LUNG","FALSE");
+insert into Fmi values ("GO28915","541719","15891411","SCRN","TRF155919","05850007B""FDMI","DNA","LUNG","FALSE");
 
-insert into Hist values ("GO28915","544487","13558311","SCRN","05850006B","S4405900","HIST","BLOCK SLIDE","LUNG","FALSE");
-insert into Hist values ("GO28915","550054","15891508","SCRN","05850007B","S4405901","HIST","BLOCK SLIDE","LUNG","FALSE");
+insert into Hist values ("GO28915","544487","13558311","SCRN","05850006B","L1630467","HIST","BLOCK SLIDE","LUNG","FALSE");
+insert into Hist values ("GO28915","550054","15891508","SCRN","05850007B","EB593671","HIST","BLOCK SLIDE","LUNG","FALSE");
 
-insert into Labc values ("GO28915","521895","11948329","Cycle 1 Day 1","SC-839608-A01","GN006071","LABC","RNA","Whole Blood","FALSE");
-insert into Labc values ("GO28915","560201","14091875","Cycle 1 Day 1","SC-839608-A02","GN008849","LABC","RNA","Whole Blood","FALSE");
+insert into Labc values ("GO28915","521895","11948329","Cycle 1 Day 1","SC-839608-A01","GN006071 0702","LABC","RNA","Whole Blood","FALSE");
+insert into Labc values ("GO28915","560201","14091875","Cycle 1 Day 1","SC-839608-A02","GN006071 0702","LABC","RNA","Whole Blood","FALSE");
 
 insert into Precision_pbmc values ("GO28915","521895","11948329","Cycle 1 Day 1","GN006071","","Precision","PBMC","Whole Blood","TRUE");
 insert into Precision_pbmc values ("GO28915","560201","14091875","Cycle 1 Day 1","GN008849","","Precision","PBMC","Whole Blood","TRUE");
